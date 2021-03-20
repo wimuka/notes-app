6c65c3888feb38a3d2b7edd3bec1a30533c69f0c
wimuka <avii08el1s@gmail.com>
2021-03-20 11:51:19 -0400

    testing server

--- server.js
+++ server.js
@@ -6,20 +6,20 @@ const middlewares = jsonServer.defaults();
 const router = jsonServer.router('db.json');
 const port = process.env.PORT || 5000;
 
-// app.use(function (req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept'
-//   );
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 app.use('/', middlewares, router);
 app.use(express.static('./db.json'));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'db.json'));
+app.get('*', function (req, res) {
+  res.sendFile(path.resolve(__dirname + '/client/build/index.html'));
 });
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
