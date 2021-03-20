0df09bef847b444bfbc6923d2a2301a51437de7f
wimuka <avii08el1s@gmail.com>
2021-03-20 11:39:34 -0400

    server edit

--- server.js
+++ server.js
@@ -1,30 +1,25 @@
 const jsonServer = require('json-server');
 const app = jsonServer.create();
-const express = require('express');
 const path = require('path');
-const router = jsonServer.router('db.json');
+const express = require('express');
 const middlewares = jsonServer.defaults();
-
-const data = require('./db');
-
-app.use(middlewares);
-app.use(data, middlewares, router);
-
-app.use(express.static('public'));
-//Without this part fetching of villagers.json doesn't work (says no access for access-control-allow-origin)
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
-app.get('/notes', function (req, res, next) {
-  res.send(data);
-});
+const router = jsonServer.router('db.json');
 const port = process.env.PORT || 5000;
 
-app.listen(port, function () {
-  console.log('Server started.......');
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept'
+//   );
+//   next();
+// });
+
+app.use('/', middlewares, router);
+app.use(express.static('./db.json'));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'db.json'));
 });
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import notesReducer from './notesReducer';
-
-//Standard Setup
-const rootReducer = combineReducers({
-  notes: notesReducer,
-});
-
-export default rootReducer;
